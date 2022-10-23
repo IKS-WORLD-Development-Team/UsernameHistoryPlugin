90be661621b84e294004f14a2cbe13a0f430510d
DESKTOP-IGEK31A\acer <mcyyy123454321@gmail.com>
2022-10-23 23:07:11 +0900

    edit: resultDto Type in RootListener

--- src/main/java/kr/iksworld/plugin/usernamehistory/root/RootListener.java
+++ src/main/java/kr/iksworld/plugin/usernamehistory/root/RootListener.java
@@ -1,8 +1,12 @@
 package kr.iksworld.plugin.usernamehistory.root;
 
 import kr.iksworld.plugin.usernamehistory.dto.event.PlayerJoinEventDto;
+import kr.iksworld.plugin.usernamehistory.dto.event.PlayerQuitEventDto;
+import kr.iksworld.plugin.usernamehistory.dto.result.GeneralMessageResultDto;
 import kr.iksworld.plugin.usernamehistory.dto.result.PlayerJoinResultDto;
+import kr.iksworld.plugin.usernamehistory.dto.result.PlayerQuitResultDto;
 import kr.iksworld.plugin.usernamehistory.listener.impl.PlayerJoinListenerImpl;
+import kr.iksworld.plugin.usernamehistory.listener.impl.PlayerQuitListenerImpl;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -18,11 +22,17 @@ public class RootListener implements Listener {
                 System.currentTimeMillis()
         );
 
-        PlayerJoinResultDto playerJoinResultDto = new PlayerJoinListenerImpl().onPlayerJoinEvent(playerJoinEventDto);
+        PlayerJoinResultDto resultDto = new PlayerJoinListenerImpl().onPlayerJoinEvent(playerJoinEventDto);
     }
 
     @EventHandler
     public void onPlayerQuitEvent(PlayerQuitEvent event) {
+        PlayerQuitEventDto playerQuitEventDto = new PlayerQuitEventDto(
+                event.getPlayer().getName(),
+                event.getPlayer().getUniqueId(),
+                System.currentTimeMillis()
+        );
 
+        PlayerQuitResultDto resultDto = new PlayerQuitListenerImpl().onPlayerQuitEvent(playerQuitEventDto);
     }
 }
