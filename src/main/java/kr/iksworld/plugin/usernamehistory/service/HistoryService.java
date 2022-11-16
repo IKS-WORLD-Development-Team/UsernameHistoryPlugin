e509f2f9008179ebf57479b0826d22c90d15abfe
DESKTOP-IGEK31A\acer <mcyyy123454321@gmail.com>
2022-11-17 01:57:00 +0900

    refactor: add get, search

--- src/main/java/kr/iksworld/plugin/usernamehistory/service/HistoryService.java
+++ src/main/java/kr/iksworld/plugin/usernamehistory/service/HistoryService.java
@@ -19,9 +19,13 @@ public interface HistoryService<K, V> {
 
     Optional<History<K, V>> get(K key, long id);
 
-    Optional<List<History<K, V>>> searchByKey(K key);
+    Optional<History<K, V>> getByKey(K key, long id);
 
-    Optional<List<History<K, V>>> searchByValue(V value);
+    Optional<History<K, V>> getByValue(V value, long id);
+
+    Optional<List<History<K, V>>> searchByKey(String keyQuery);
+
+    Optional<List<History<K, V>>> searchByValue(String valueQuery);
 
     // DELETE
 
--- src/main/java/kr/iksworld/plugin/usernamehistory/service/impl/UsernameHistoryService.java
+++ src/main/java/kr/iksworld/plugin/usernamehistory/service/impl/UsernameHistoryService.java
@@ -24,13 +24,23 @@ public class UsernameHistoryService<K, V> implements HistoryService<K, V> {
     }
 
     @Override
-    public Optional<List<History<K, V>>> searchByKey(K key) {
-        return null;
+    public Optional<History<K, V>> getByKey(K key, long id) {
+        return Optional.empty();
     }
 
     @Override
-    public Optional<List<History<K, V>>> searchByValue(V value) {
-        return null;
+    public Optional<History<K, V>> getByValue(V value, long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<List<History<K, V>>> searchByKey(String keyQuery) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<List<History<K, V>>> searchByValue(String valueQuery) {
+        return Optional.empty();
     }
 
     @Override
