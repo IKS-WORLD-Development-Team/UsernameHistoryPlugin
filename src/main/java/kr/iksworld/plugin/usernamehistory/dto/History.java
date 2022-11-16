ca93d18dbc9690f2f175b35a346e9da8b7dd0e00
DESKTOP-IGEK31A\acer <mcyyy123454321@gmail.com>
2022-11-17 01:47:03 +0900

    refactor: edit generic (T key, K value -> K key, V value)

--- src/main/java/kr/iksworld/plugin/usernamehistory/dto/History.java
+++ src/main/java/kr/iksworld/plugin/usernamehistory/dto/History.java
@@ -1,26 +1,26 @@
 package kr.iksworld.plugin.usernamehistory.dto;
 
-public class History<T, K> {
-    private T key;
-    private K value;
+public class History<K, V> {
+    private K key;
+    private V value;
     private long time;
 
-    public History(T key, K value) {
+    public History(K key, V value) {
         this(key, value, 0);
         time = System.currentTimeMillis();
     }
 
-    public History(T key, K value, long time) {
+    public History(K key, V value, long time) {
         this.key = key;
         this.value = value;
         this.time = time;
     }
 
-    public T getKey() {
+    public K getKey() {
         return key;
     }
 
-    public K getValue() {
+    public V getValue() {
         return value;
     }
 
