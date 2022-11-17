56b1903243944b307e1259b87b710210858e877e
DESKTOP-IGEK31A\acer <mcyyy123454321@gmail.com>
2022-11-17 21:45:06 +0900

    add: value in HistoryType

--- src/main/java/kr/iksworld/plugin/usernamehistory/dto/HistoryType.java
+++ src/main/java/kr/iksworld/plugin/usernamehistory/dto/HistoryType.java
@@ -1,5 +1,15 @@
 package kr.iksworld.plugin.usernamehistory.dto;
 
 public enum HistoryType {
-    JOIN, QUIT;
+    JOIN((byte) 0), QUIT((byte) 1);
+
+    private byte value;
+
+    HistoryType(byte value) {
+        this.value = value;
+    }
+
+    public byte byteValue() {
+        return value;
+    }
 }
